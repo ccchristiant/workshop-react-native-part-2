2f4c8a7c2fd45d3a0b88186e4e0a57b58d2290c5
ccchristiant <christian.tran@epitech.eu>
2023-02-18 00:35:06 +0100

    [ADD] Part 1

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ActivityIndicator, Image, ScrollView } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios'
+
+import CurrentWeather from './components/CurrentWeather'
+
+// replace the API_KEY with your own
+const API_URL = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={API_KEY}=fr&units=metric`
+
+export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const getCoordinates = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        return
+      }
+
+      const userLocation = await Location.getCurrentPositionAsync()
+      getWeather(userLocation)
+    }
+
+    getCoordinates()
+  }, [])
+
+  const getWeather = async (location) => {
+    try {
+      const response = await axios.get(API_URL(location.coords.latitude, location.coords.longitude))
+
+      setData(response.data)
+      setLoading(false)
+    } catch(e) {
+      console.log("Error in getWeather")
+    }
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <CurrentWeather data={data} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E2E6E1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+});
